815d7742b77fa1c8e9bcaaeeeefa1ac52e8914ba
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2021-09-26 19:33:18 +0530

    Admin Object Name Done

--- phyProject/phyApp/models.py
+++ phyProject/phyApp/models.py
@@ -10,8 +10,15 @@ class Simulation_Project(models.Model):
     youtube_link = models.CharField(max_length=1000)
     image = models.ImageField(upload_to='images' , null=True)
 
+    def __str__(self):
+        return self.project_name
+
 class Video_Project(models.Model):
     project_name = models.CharField(max_length=200)
     author_name = models.CharField(max_length=200)
     youtube_link = models.CharField(max_length=1000)
     image = models.CharField(max_length=1000)
+
+
+    def __str__(self):
+        return self.project_name
